2e6311a598bb072a148621f5881507cdf0076e53
Andrew Kramer <andrew@jmpesp.org>
2017-09-10 16:24:35 +0000

    remove users(id) column to allow username to be a primary key

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -47,7 +47,6 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(64) DEFAULT NULL,
   `score` int(11) DEFAULT '9999',
   PRIMARY KEY (`username`)
