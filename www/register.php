e1686818eec406e8185350927cb4fae1e0ad8c61
Andrew Kramer <andrew@jmpesp.org>
2017-09-12 15:42:00 +0000

    Dont refresh on registration page

--- www/register.php
+++ www/register.php
@@ -78,7 +78,6 @@ if(isset($_POST["submit"])) {
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
 	<!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
-	<meta http-equiv="refresh" content="10">
 	<script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
 
