31664729f6245f5bf97f248ffc6edf05d33cbf01
Andrew Kramer <andrew@jmpesp.org>
2017-09-12 17:02:04 +0000

    Add syscall filter to runner

--- runner/runner.c
+++ runner/runner.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include <seccomp.h>
+#include <linux/seccomp.h>
+
 #define SU_UID 1000
 #define SU_GID 1000
 
@@ -8,11 +11,25 @@
 
 int main() {
 
-//	chroot(getenv("PWD"));
+	scmp_filter_ctx ctx;
+
+	ctx = seccomp_init(SCMP_ACT_KILL); // default action: kill
+
+	seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(rt_sigreturn), 0);
+	seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(exit), 0);
+	seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(read), 0);
+	seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(write), 0);
+	seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(exit_group), 0);
+	seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(open), 0);
+	seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(close), 0);
+
+	seccomp_load(ctx);
 
 	setgroups(0);
 	setgid(SU_GID);
 	setuid(SU_UID);
+
+//	chroot(getenv("PWD"));
 
 	execl("/usr/bin/timeout", "/usr/bin/timeout", "-s", "SIGKILL", TIMEOUT, "./code", NULL);
 
